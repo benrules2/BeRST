dc9bbd9ae1c45b173de4d0b8fb458fda82024696
Ben Eagan <bceagan@gmail.com>
2021-06-03 16:15:11 -0400

    Add runner script for GabaPentin study

---
+++ log_cats.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Here we go!"
+gpio -g write 4 1
+rclone mount cateye_backup:cateye ~/cateye_backup --allow-non-empty --vfs-cache-mode writes --daemon
+CATEYE_BACKUP=~/cateye_backup python3 detect_cats.py --stream --roi -v
+echo "C'est Finis"
