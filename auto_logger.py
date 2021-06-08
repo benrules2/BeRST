89327afaffe48f650d577ad553102f9ded0c3ba6
Ben Eagan <bceagan@gmail.cm>
2021-06-08 09:54:14 -0400

    Improvements to auto_logger

--- auto_logger.py
+++ auto_logger.py
@@ -3,32 +3,30 @@ import os
 import log
 import json
 
+REMOTE_CONFIG = "remote_config.json"
 class AutoLogger:
     def __init__(self, custom_data_file = None):
         #TODO - add proper cli parser arguments for this instead of environment variable)
+        self.custom_data_file = None
         
-        if os.path.exists("remote_config.json"):
-            conf = open("remote_config.json", 'r')
+        if os.path.exists(REMOTE_CONFIG):
+            conf = open(REMOTE_CONFIG, 'r')
             config = json.load(conf)
             remote = os.path.join(config['backup_dir'], config['computer_id'])
             log.info("Attemping to mounting OneDrive:cateye to {}".format(config['backup_dir']))
             os.system("rclone mount cateye:cateye {} --allow-non-empty --vfs-cache-mode writes --daemon".format(config['backup_dir']))
 
-        elif os.environ("CATEYE_BACKUP"):
-            remote = os.environ("CATEYE_BACKUP")
-        
-        if remote is None:
-            log.error("Empty CATEYE_BACKUP env - writing to log folder locally")
+        elif custom_data_file is None:
+            raise Exception("Missing Config", "Run 'python3 generate_runner.py' to create remote backup config file")
+
+        else:
+            log.info("User specified output file provided, logging to " + self.custom_data_file)
             remote = "log"
+            self.custom_data_file = custom_data_file
 
         self.log_dir = remote
         self.log_name = None
         self.data_file = None
-        self.custom_data_file = None
-        
-        if custom_data_file:
-            self.custom_data_file = custom_data_file
-            log.info("User specified output file provided, logging to " + self.custom_data_file)
         
         if not os.path.isdir(self.log_dir):
             log.info("Path does not exist, creating {}".format(self.log_dir))
@@ -53,9 +51,11 @@ class AutoLogger:
                 self.data_file = open(name, 'a', buffering=1)
                 log.info("Appending output {} {}".format(name, self.log_name))
             else:
-                self.data_file = open(name, 'w', buffering=1)
+                if not os.path.exists(self.log_dir):
+                    os.makedirs(self.log_dir)
+                self.data_file = open(name, 'w+', buffering=1)
                 log.info("Initializing output {} {}".format(name, self.log_name))
-                self.data_file.write("#id,frame_idx,timestamp\n")
+                self.data_file.write("#id, frame_count, timestamp, midpoint X coord, midpoint Y coord, roi_idx")
             self.log_name = local_name
         
         return self.data_file
